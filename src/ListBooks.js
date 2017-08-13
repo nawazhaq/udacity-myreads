b21f8b050c3b44de70f29b92513996f9d90841bc
nasheikh <nasheikh@visa.com>
2017-08-13 00:38:40 -0700

    feat: show books list categories , update book category and search books

---
+++ src/ListBooks.js
@@ -0,0 +1,107 @@
+import React,{Component} from 'react';
+
+class ListBooks extends Component{
+
+    render(){
+        const { books , onSelectBook } = this.props;
+        const currently_read = books.filter((book) => book.shelf === 'currentlyReading');
+        const want_read = books.filter((book) => book.shelf === 'wantToRead');
+        const already_read = books.filter((book) => book.shelf === 'read');
+
+
+        return (
+            <div>
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">Currently Reading</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+
+                        {currently_read.map((book) =>(
+
+                            <li key={book.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url({book.previewLink})' }}></div>
+                                        <div className="book-shelf-changer">
+                                            <select onChange={(event) => onSelectBook(book,event.target.value)}>
+                                                <option value="none" disabled>Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div className="book-title">{book.title}</div>
+                                    <div className="book-authors">{book.authors[0]}</div>
+                                </div>
+                            </li>
+                        )
+                        )  }
+
+                    </ol>
+                </div>
+            </div>
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">Want to Read</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {want_read.map((book) =>(
+
+                                <li key={book.id}>
+                                    <div className="book">
+                                        <div className="book-top">
+                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                                            <div className="book-shelf-changer">
+                                                <select onChange={(event) => onSelectBook(book,event.target.value)}>
+                                                    <option value="none" disabled>Move to...</option>
+                                                    <option value="currentlyReading">Currently Reading</option>
+                                                    <option value="wantToRead">Want to Read</option>
+                                                    <option value="read">Read</option>
+                                                    <option value="none">None</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="book-title">{book.title}</div>
+                                        <div className="book-authors">{book.authors[0]}</div>
+                                    </div>
+                                </li>
+                            )
+                        )  }
+                    </ol>
+                </div>
+            </div>
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">Read</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {already_read.map((book) =>(
+
+                                <li key={book.id}>
+                                    <div className="book">
+                                        <div className="book-top">
+                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                                            <div className="book-shelf-changer">
+                                                <select onChange={(event) => onSelectBook(book,event.target.value)}>
+                                                    <option value="none" disabled>Move to...</option>
+                                                    <option value="currentlyReading">Currently Reading</option>
+                                                    <option value="wantToRead">Want to Read</option>
+                                                    <option value="read">Read</option>
+                                                    <option value="none">None</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="book-title">{book.title}</div>
+                                        <div className="book-authors">{book.authors[0]}</div>
+                                    </div>
+                                </li>
+                            )
+                        )  }
+                    </ol>
+                </div>
+            </div>
+        </div>)
+    }
+}
+
+export default ListBooks;
