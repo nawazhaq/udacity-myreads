976ddd34370db75cbeda8aef25161790053f0649
nasheikh <nasheikh@visa.com>
2017-08-19 11:39:30 -0700

    feat: show books list categories , update books category and search books

--- src/App.js
+++ src/App.js
@@ -2,16 +2,10 @@ import React ,{Component} from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListBooks from './ListBooks'
+import { Route, Link } from 'react-router-dom'
 
 class BooksApp extends Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: true,
     books:[],
     query:""
   }
@@ -50,53 +44,48 @@ class BooksApp extends Component {
     const booksList = this.state.books;
     return (
         <div className="app">
-          {this.state.showSearchPage ? (
+
+          <Route path="/search" render={({history})=>(
               <div className="search-books">
                 <div className="search-books-bar">
-                  <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                  <Link to="/" className="close-search">Close</Link>
                   <div className="search-books-input-wrapper">
-                    {/*
-                     NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                     You can find these search terms here:
-                     https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                     However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                     you don't find a specific author or title. Every search is limited by search terms.
-                     */}
                     <input type="text" placeholder="Search by title or author"
                            onChange={(event)=> this.searchBook(event.target.value,maxResult)}/>
-
                   </div>
                 </div>
                 <div className="search-books-results">
                   <ol className="books-grid">
                     {booksList.map((book) =>(
-                        <li key={book.id}>
-                          <div className="book">
-                            <div className="book-top">
-                              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url({book.previewLink})' }}></div>
-                              <div className="book-shelf-changer">
-                                <select onChange={(event) => this.updateShelf(book,event.target.value)}>
-                                  <option value="none" disabled>Move to...</option>
-                                  <option value="currentlyReading">Currently Reading</option>
-                                  <option value="wantToRead">Want to Read</option>
-                                  <option value="read">Read</option>
-                                  <option value="none">None</option>
-                                </select>
+                            <li key={book.id}>
+                              <div className="book">
+                                <div className="book-top">
+                                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                  <div className="book-shelf-changer">
+                                    <select value="none" onChange={(event) => this.updateShelf(book,event.target.value)}>
+                                      <option value="none" disabled>Move to...</option>
+                                      <option value="currentlyReading">Currently Reading</option>
+                                      <option value="wantToRead">Want to Read</option>
+                                      <option value="read">Read</option>
+                                      <option value="none">None</option>
+                                    </select>
+                                  </div>
+                                </div>
+                                <div className="book-title">{book.title}</div>
+                                <div className="book-authors">{book.authors[0]}</div>
                               </div>
-                            </div>
-                            <div className="book-title">{book.title}</div>
-                            <div className="book-authors">{book.authors[0]}</div>
-                          </div>
-                        </li>
-                    )
+                            </li>
+                        )
                     ) }
 
 
                   </ol>
                 </div>
               </div>
-          ) : (
+
+          )}
+          />
+          <Route exact path="/" render={()=>(
               <div className="list-books">
                 <div className="list-books-title">
                   <h1>MyReads</h1>
@@ -105,10 +94,12 @@ class BooksApp extends Component {
                   <ListBooks books={this.state.books} onSelectBook={this.updateShelf}/>
                 </div>
                 <div className="open-search">
-                  <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+                  <Link to="/search">
+                    Add a book
+                  </Link>
                 </div>
               </div>
-          )}
+          )} />
         </div>
     )
   }
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -21,9 +21,9 @@ class ListBooks extends Component{
                             <li key={book.id}>
                                 <div className="book">
                                     <div className="book-top">
-                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url({book.previewLink})' }}></div>
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
                                         <div className="book-shelf-changer">
-                                            <select onChange={(event) => onSelectBook(book,event.target.value)}>
+                                            <select value="currentlyReading" onChange={(event) => onSelectBook(book,event.target.value)}>
                                                 <option value="none" disabled>Move to...</option>
                                                 <option value="currentlyReading">Currently Reading</option>
                                                 <option value="wantToRead">Want to Read</option>
@@ -51,9 +51,9 @@ class ListBooks extends Component{
                                 <li key={book.id}>
                                     <div className="book">
                                         <div className="book-top">
-                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                                             <div className="book-shelf-changer">
-                                                <select onChange={(event) => onSelectBook(book,event.target.value)}>
+                                                <select value="wantToRead" onChange={(event) => onSelectBook(book,event.target.value)}>
                                                     <option value="none" disabled>Move to...</option>
                                                     <option value="currentlyReading">Currently Reading</option>
                                                     <option value="wantToRead">Want to Read</option>
@@ -80,9 +80,9 @@ class ListBooks extends Component{
                                 <li key={book.id}>
                                     <div className="book">
                                         <div className="book-top">
-                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                                             <div className="book-shelf-changer">
-                                                <select onChange={(event) => onSelectBook(book,event.target.value)}>
+                                                <select value="read" onChange={(event) => onSelectBook(book,event.target.value)}>
                                                     <option value="none" disabled>Move to...</option>
                                                     <option value="currentlyReading">Currently Reading</option>
                                                     <option value="wantToRead">Want to Read</option>
